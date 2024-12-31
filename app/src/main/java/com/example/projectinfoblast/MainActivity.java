fa9bf3f99c82a7339bb7101d36ef02ee87008e68
Aceceh <aisyiskandar1177@gmail.com>
2024-12-31 15:57:49 +0800

    initial commit

---
+++ app/src/main/java/com/example/projectinfoblast/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.projectinfoblast;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView recyclerView;
+    private Toolbar mToolBar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolBar); //add tool bar to main activity
+        getSupportActionBar().setTitle("Home");
+
+
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+        // three bar menu line
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void UserMenuSelector(MenuItem item) {
+
+        if (item.getItemId() == R.id.nav_profile) {
+            Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+        }
+        if (item.getItemId() == R.id.nav_addpost) {
+            Toast.makeText(MainActivity.this, "Add New Post", Toast.LENGTH_SHORT).show();
+
+        }if (item.getItemId() == R.id.nav_home) {
+            Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+
+        }if (item.getItemId() == R.id.nav_setting) {
+            Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+
+        }if (item.getItemId() == R.id.nav_logout) {
+            Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
